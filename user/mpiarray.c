dfe197cc9b80ce4806854bc1b3cc658aca44a3b3
cool0802 <35596975+cool0802@users.noreply.github.com>
2019-12-13 20:03:42 +0530

    confusion matrix

--- user/mpiarray.c
+++ user/mpiarray.c
@@ -1,27 +1,32 @@
-#include<mpi.h>
 #include<stdio.h>
-int main(int argc, char ** argv)
+#include<mpi.h>
+#include<stdlib.h>
+int main(int argc, char * argv[])
 {
+int i;
+int nitems = 10;
 int mynode, totalnodes;
-int sum,startval,endval,accum;
 MPI_Status status;
+double * array;
 MPI_Init(NULL,NULL);
 MPI_Comm_size(MPI_COMM_WORLD, &totalnodes);
 MPI_Comm_rank(MPI_COMM_WORLD, &mynode);
-sum = 0;
-startval = 1000*(mynode/totalnodes)+1;
-endval = 1000*(mynode+1)/totalnodes;
-for(int i=startval;i<=endval;i=i+1)
-sum = sum + i;
-if(mynode!=0)
-MPI_Send(&sum,1,MPI_INT,0,1,MPI_COMM_WORLD);
-else
-for(int j=1;j<totalnodes;j=j+1){
-MPI_Recv(&accum,1,MPI_INT,j,1,MPI_COMM_WORLD,
-&status);
-sum = sum + accum;
-}
+array =(double *) malloc(sizeof(double)*nitems);
 if(mynode == 0)
-printf( "The sum from 1 to 1000 is:%d",sum );
+{
+for(i=0;i<nitems;i++)
+array[i] += (double) i;
+}
+if(mynode==0)
+for(i=1;i<totalnodes;i++)
+MPI_Send(array,nitems,MPI_DOUBLE,i,1,MPI_COMM_WORLD);
+else
+MPI_Recv(array,nitems,MPI_DOUBLE,0,1,MPI_COMM_WORLD,
+&status);
+for(i=0;i<nitems;i++)
+{
+printf("Processor %d",mynode);
+printf(": array[%d] = %f\n",i,array[i]);
+}
 MPI_Finalize();
 }
